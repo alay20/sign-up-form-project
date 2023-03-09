1eb06df7d0c6e8ed7ad61be3799914abb98887bf
Arthur Lay <arthur.lay@gmail.com>
2023-03-08 22:07:56 -0500

    Form will not submit until both passwords match. Fixed typo in password do not match message.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var password1 = document.getElementById("password1");
 var password2 = document.getElementById("password2");
 var pwError = document.querySelector(".error");
+var submitBtn = document.querySelector(".submit-btn")
 
 password1.addEventListener('input', pwCheck);
 password2.addEventListener('input', pwCheck);
 
 function pwCheck() {
+    submitBtn.disabled = false;
     if (password1.value === password2.value) {
         password1.style.borderColor = "green";
         password2.style.borderColor = "green";
@@ -14,7 +16,8 @@ function pwCheck() {
         console.log("pw do not match");
         password1.style.borderColor = "red";
         password2.style.borderColor = "red";
-        pwError.textContent = "*Passwords to not match";
+        pwError.textContent = "*Passwords do not match";
+        submitBtn.disabled = true;
     }
 }
 
